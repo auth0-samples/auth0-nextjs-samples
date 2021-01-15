21295f93041ad261d01a9c5d146d809a96a2f733
Rita Zerrizuela <zeta@widcket.com>
2021-01-15 15:43:24 -0300

    Remove prefixes and suffixes from dummy settings

--- Sample-01/tests/environment.js
+++ Sample-01/tests/environment.js
@@ -1,5 +1,5 @@
-process.env.AUTH0_SECRET = '__test_session_secret__';
+process.env.AUTH0_SECRET = 'session_secret';
 process.env.AUTH0_ISSUER_BASE_URL = 'https://acme.auth0.local';
 process.env.AUTH0_BASE_URL = 'http://www.acme.com/';
-process.env.AUTH0_CLIENT_ID = '__test_client_id__';
-process.env.AUTH0_CLIENT_SECRET = '__test_client_secret__';
+process.env.AUTH0_CLIENT_ID = 'client_id';
+process.env.AUTH0_CLIENT_SECRET = 'client_secret';
