e4cd0e36fa8eb44b1d14dd6cb1d1d918f75c8764
Rita Zerrizuela <zeta@widcket.com>
2021-03-10 19:02:23 -0300

    Bump SDK version to v1.2.0

--- Sample-01/tests/components/Layout.test.jsx
+++ Sample-01/tests/components/Layout.test.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 
+import { withUserProvider } from '../fixtures';
 import Layout from '../../components/Layout';
 
 describe('Layout', () => {
   it('should render without crashing', async () => {
-    render(<Layout>Text</Layout>);
+    render(<Layout>Text</Layout>, { wrapper: withUserProvider({ user: undefined }) });
 
-    expect(screen.getByTestId('layout')).toBeInTheDocument();
+    await waitFor(() => expect(screen.getByTestId('layout')).toBeInTheDocument());
     expect(screen.getByTestId('navbar')).toBeInTheDocument();
     expect(screen.getByText('Text')).toBeInTheDocument();
     expect(screen.getByTestId('footer')).toBeInTheDocument();
