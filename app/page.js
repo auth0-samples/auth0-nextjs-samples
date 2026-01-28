e30e001b406ccd11b0ee4d7ff8c688364971f73d
Nicolas Garcia <nicolas.garcia@okta.com>
2026-01-28 13:29:28 -0300

    improve page.js explanations and highlighting

--- app/page.js
+++ app/page.js
@@ -1,14 +1,23 @@
 import { auth0 } from "@/lib/auth0";
 
 export default async function Home() {
+  // Check if user is authenticated
+  /* highlight-start session */
   const session = await auth0.getSession();
+  /* highlight-end session */
 
   if (!session) {
     return (
       <>
+        {/* Redirects to Auth0 to sign up */}
+        {/* highlight-start signup */}
         <a href="/auth/login?screen_hint=signup">Signup</a>
+        {/* highlight-end signup */}
         <br />
+        {/* Redirects to Auth0 to log in */}
+        {/* highlight-start login */}
         <a href="/auth/login">Login</a>
+        {/* highlight-end login */}
       </>
     );
   }
@@ -17,11 +26,16 @@ export default async function Home() {
     <>
       <p>Logged in as {session.user.email}</p>
 
+      {/* Display user info (name, email, etc.) */}
+      {/* highlight-start profile */}
       <h1>User Profile</h1>
-
       <pre>{JSON.stringify(session.user, null, 2)}</pre>
+      {/* highlight-end profile */}
 
+      {/* Ends the session and redirects to Auth0 to log out */}
+      {/* highlight-start logout */}
       <a href="/auth/logout">Logout</a>
+      {/* highlight-end logout */}
     </>
   );
 }
