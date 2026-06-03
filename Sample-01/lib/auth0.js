21fd60a6abba664fe9e49570f0d5496d47a67685
Alex Clarke <aclar48@users.noreply.github.com>
2026-06-03 14:47:43 -0400

    Update auth0.js

--- Sample-01/lib/auth0.js
+++ Sample-01/lib/auth0.js
@@ -8,7 +8,7 @@ export const auth0 = new Auth0Client({
   // clientId: process.env.AUTH0_CLIENT_ID,
   // clientSecret: process.env.AUTH0_CLIENT_SECRET,
   // secret: process.env.AUTH0_SECRET,
-  appBaseUrl: process.env.VERCEL_URL ? `https://${process.env.VERCEL_URL}` : process.env.APP_BASE_URL,
+  appBaseUrl: process.env.VERCEL_PROJECT_PRODUCTION_URL ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : process.env.APP_BASE_URL,
   authorizationParameters: {
     // In v4, the AUTH0_SCOPE and AUTH0_AUDIENCE environment variables are no longer automatically picked up by the SDK.
     // Instead, we need to provide the values explicitly.
