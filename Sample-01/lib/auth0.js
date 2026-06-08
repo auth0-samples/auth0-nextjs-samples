04554778c6364a8e8000c22f70432bf46f09f772
Alex Clarke <aclar48@users.noreply.github.com>
2026-06-08 14:57:23 -0400

    Update auth0.js

--- Sample-01/lib/auth0.js
+++ Sample-01/lib/auth0.js
@@ -8,7 +8,6 @@ export const auth0 = new Auth0Client({
   // clientId: process.env.AUTH0_CLIENT_ID,
   // clientSecret: process.env.AUTH0_CLIENT_SECRET,
   // secret: process.env.AUTH0_SECRET,
-  appBaseUrl: isBrowser ? (new URL(window.location.href).origin) : process.env.APP_BASE_URL,
   authorizationParameters: {
     // In v4, the AUTH0_SCOPE and AUTH0_AUDIENCE environment variables are no longer automatically picked up by the SDK.
     // Instead, we need to provide the values explicitly.
