820f71760fe0b898462008125acec2a3faf4dfb4
Alex Clarke <aclar48@users.noreply.github.com>
2026-06-09 14:17:19 -0400

    Update auth0.js

--- Sample-01/lib/auth0.js
+++ Sample-01/lib/auth0.js
@@ -1,5 +1,4 @@
 import { Auth0Client } from "@auth0/nextjs-auth0/server";
-const isBrowser = (typeof window !== 'undefined');
 // Initialize the Auth0 client 
 export const auth0 = new Auth0Client({
   // Options are loaded from environment variables by default
@@ -8,6 +7,7 @@ export const auth0 = new Auth0Client({
   // clientId: process.env.AUTH0_CLIENT_ID,
   // clientSecret: process.env.AUTH0_CLIENT_SECRET,
   // secret: process.env.AUTH0_SECRET,
+  appBaseUrl: process.env.APP_BASE_URL,
   authorizationParameters: {
     // In v4, the AUTH0_SCOPE and AUTH0_AUDIENCE environment variables are no longer automatically picked up by the SDK.
     // Instead, we need to provide the values explicitly.
